d8d8e1abf4b604ea372aafb24a4b3d7c67124290
Michael Herold <opensource@michaeljherold.com>
2020-03-15 14:21:50 -0500

    Reduce some duplication

--- lib/trace_method/helpers.rb
+++ lib/trace_method/helpers.rb
@@ -3,24 +3,12 @@
 module TraceMethod
   module Helpers
     def trace_methods(*methods)
-      raise UnspecifiedMethods, 'You must give at least one method to trace' if methods.empty?
-
-      if (mod = ancestors.find(&:__trace_method__?))
-        Module.define_methods_on(mod, *methods)
-      else
-        prepend Module.new(*methods)
-      end
+      add_traces_for(self, methods)
     end
     alias trace_method trace_methods
 
     def trace_singleton_methods(*methods)
-      raise UnspecifiedMethods, 'You must give at least one method to trace' if methods.empty?
-
-      if (mod = singleton_class.ancestors.find(&:__trace_method__?))
-        Module.define_methods_on(mod, *methods)
-      else
-        singleton_class.prepend Module.new(*methods)
-      end
+      add_traces_for(singleton_class, methods)
     end
     alias trace_singleton_method trace_singleton_methods
 
@@ -46,6 +34,16 @@ module TraceMethod
 
     def __adapter__
       @__adapter__ ||= TraceMethod.config.adapter
+    end
+
+    def add_traces_for(base, methods)
+      raise UnspecifiedMethods, 'You must give at least one method to trace' if methods.empty?
+
+      if (mod = base.ancestors.find(&:__trace_method__?))
+        Module.define_methods_on(mod, *methods)
+      else
+        base.prepend Module.new(*methods)
+      end
     end
   end
 end
